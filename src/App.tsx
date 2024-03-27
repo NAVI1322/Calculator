3a2d5f802392fe86916e7ca931b509c132386f6d
Gurtaj06 <sonicgurtaj079@gmail.com>
2024-03-26 20:09:56 -0400

    added onClick funtionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,40 @@
 import { Button } from "./components/ui/button"
-import "./App.css"
 import { Input } from "./components/ui/input"
+import { useState } from "react";
 
 function App() {
-  const list = ['(', ')', '%', 'AC', 7, 8, 9, '÷', 6, 5, 4, 'x', 3, 2, 1, '-', 0, '.', '=', '+']
+  const [inputValue, setInputValue] = useState("");
+  const list = ['(', ')', '%', 'AC', 7, 8, 9, '÷', 6, 5, 4, 'x', 3, 2, 1, '-', 0, '.', '=', '+'];
+
+  const handleClick = (value: any) => {
+    if (value === "=") {
+      evaluate();
+    }
+    else if (value === "AC") {
+      setInputValue("")
+    } else {
+      setInputValue(inputValue === "0" ? value : inputValue + value);
+    }
+  };
+  const evaluate = () => {
+    try {
+      const result = eval(inputValue.replace(/x/g, '*').replace(/÷/g, '/'));
+      setInputValue(result.toString());
+    } catch (error) {
+      setInputValue("Error");
+    }
+  };
+
   return (
     <div className="p-12 min-w-72 max-w-2xl">
-      <Input className="min-w-72 max-w-2xl text-right my-4" value={0} />
+      <Input className="min-w-72 max-w-2xl text-right my-4" readOnly value={inputValue} />
 
       <div className="grid grid-cols-4 gap-4">
         {
-          list.map(list => <Button variant={"secondary"}>{list}</Button>)
+          list.map(listItem => <Button variant={"secondary"} value={listItem} key={listItem} onClick={() => handleClick(listItem)}>{listItem}</Button>)
         }
       </div>
-    </div>
+    </div >
   )
 }
 
